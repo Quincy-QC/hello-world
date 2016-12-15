7875e73f02d6590707affe0de3d63072230e4631
Quincy <Quincy@sinodeMac-mini.local>
2016-12-15 15:29:28 +0800

    first commit

---
+++ hello world/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello world
+//
+//  Created by Quincy on 16/12/15.
+//  Copyright © 2016年 Quincy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hello world/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello world
+//
+//  Created by Quincy on 16/12/15.
+//  Copyright © 2016年 Quincy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
